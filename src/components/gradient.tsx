3b410b16fbe4c1723ccd260d77b128010447b751
silhquette <muhammadqiyam03@gmail.com>
2025-09-01 21:33:49 +0700

    adjust gradient color

--- src/components/gradient.tsx
+++ src/components/gradient.tsx
@@ -9,7 +9,7 @@ export function Gradient({
       {...props}
       className={clsx(
         className,
-        'bg-linear-115 from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff] sm:bg-linear-145',
+        'bg-linear-115 from-[#FFFFFF] from-28% via-[#FFFFC2] via-70% to-[#FEF200] sm:bg-linear-145',
       )}
     />
   )
