9a9d16be3a81a08457973cfd2346c0cbb76cbdf3
silhquette <muhammadqiyam03@gmail.com>
2025-09-01 20:52:35 +0700

    overview about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,8 @@ import { Map } from '@/components/map'
 import { Navbar } from '@/components/navbar'
 import { Screenshot } from '@/components/screenshot'
 import { Testimonials } from '@/components/testimonials'
-import { Heading, Subheading } from '@/components/text'
+import { Heading, Lead, Subheading } from '@/components/text'
+import { AnimatedNumber } from '@/components/animated-number'
 import { ChevronRightIcon } from '@heroicons/react/16/solid'
 import type { Metadata } from 'next'
 
@@ -35,15 +36,90 @@ function Hero() {
           <p className="mt-8 max-w-xl text-xl/7 font-medium text-gray-950/75 sm:text-2xl/8">
             We empower your brand with innovative cosmetics tailored to your needs.
           </p>
-          <div className="mt-12 flex flex-col gap-x-6 gap-y-4 sm:flex-row">
-            <Button href="#">Get started</Button>
-            <Button variant="secondary" href="/pricing">
-              See pricing
-            </Button>
-          </div>
         </div>
       </Container>
     </div>
+  )
+}
+
+function Header() {
+  return (
+    <Container className="mt-16">
+      <Heading as="h1">Empowering Beauty Brands to Thrive.</Heading>
+      <Lead className="mt-6 max-w-3xl">
+        Our mission is to revolutionize the cosmetics industry by providing innovative, high-quality formulations that elevate your brand.
+      </Lead>
+      <section className="mt-16 grid grid-cols-1 lg:grid-cols-2 lg:gap-12">
+        <div className="max-w-lg">
+          <h2 className="text-2xl font-medium tracking-tight">Behind the Brand</h2>
+          <p className="mt-6 text-sm/6 text-gray-600">
+            PT Ratu Bio Indonesia was established in January 2015 in Bandung, Indonesia, with a commitment to high-quality cosmetic manufacturing solutions tailored to our clients' needs. Starting with a passionate team, we have successfully launched over 200 products, growing into a reputable partner in the beauty industry. Our advanced facility ensures quality and compliance while we work closely with clients to bring their visions to life. With a focus on sustainability and ethical practices, we aim to lead the industry in creating responsible beauty products.
+          </p>
+        </div>
+        <div className="lg:pt-0 pt-20 lg:row-span-2 lg:-mr-16 xl:mr-auto">
+          <div className="-mx-8 grid grid-cols-2 gap-4 sm:-mx-16 sm:grid-cols-4 lg:mx-0 lg:grid-cols-2 lg:gap-4 xl:gap-8">
+            <div className="aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10">
+              <img
+                alt=""
+                src="/company/1.jpg"
+                className="block size-full object-cover"
+              />
+            </div>
+            <div className="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10 lg:-mt-32">
+              <img
+                alt=""
+                src="/company/2.jpg"
+                className="block size-full object-cover"
+              />
+            </div>
+            <div className="aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10">
+              <img
+                alt=""
+                src="/company/3.jpg"
+                className="block size-full object-cover"
+              />
+            </div>
+            <div className="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10 lg:-mt-32">
+              <img
+                alt=""
+                src="/company/4.jpg"
+                className="block size-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="max-lg:mt-16 lg:col-span-1">
+          <Subheading>Results That Matter</Subheading>
+          <hr className="mt-6 border-t border-gray-200" />
+          <dl className="mt-6 grid grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2">
+            <div className="flex flex-col gap-y-2 border-b border-dotted border-gray-200 pb-4">
+              <dt className="text-sm/6 text-gray-600">Products Launched</dt>
+              <dd className="order-first text-6xl font-medium tracking-tight">
+                <AnimatedNumber start={100} end={150} />
+              </dd>
+            </div>
+            <div className="flex flex-col gap-y-2 border-b border-dotted border-gray-200 pb-4">
+              <dt className="text-sm/6 text-gray-600">Units Produced Annually</dt>
+              <dd className="order-first text-6xl font-medium tracking-tight">
+                <AnimatedNumber start={9000} end={10000} />K
+              </dd>
+            </div>
+            <div className="flex flex-col gap-y-2 max-sm:border-b max-sm:border-dotted max-sm:border-gray-200 max-sm:pb-4">
+              <dt className="text-sm/6 text-gray-600">Clients Served</dt>
+              <dd className="order-first text-6xl font-medium tracking-tight">
+                <AnimatedNumber start={10} end={50} decimals={1} />M
+              </dd>
+            </div>
+            <div className="flex flex-col gap-y-2">
+              <dt className="text-sm/6 text-gray-600">Global Sales</dt>
+              <dd className="order-first text-6xl font-medium tracking-tight">
+                $<AnimatedNumber start={1} end={5} />M
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </section>
+    </Container>
   )
 }
 
@@ -187,11 +263,9 @@ export default function Home() {
         <Container className="mt-10">
           <LogoCloud />
         </Container>
-        <div className="bg-linear-to-b from-white from-50% to-gray-100 py-32">
-          <FeatureSection />
-          <BentoSection />
+        <div className='pt-12'>
+          <Header />
         </div>
-        <DarkBentoSection />
       </main>
       <Testimonials />
       <Footer />
